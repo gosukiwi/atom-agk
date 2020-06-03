f483489b08b59750dc9b0b80795542d081abef2f
Federico Ramirez <fedra.arg@gmail.com>
2020-06-03 18:11:04 -0300

    Refactor

--- spec/debugger/debugger-spec.js
+++ spec/debugger/debugger-spec.js
@@ -3,14 +3,13 @@ import { CompositeDisposable } from 'atom'
 import Debugger from '../../lib/debugger/debugger'
 import Runner from '../../lib/debugger/runner'
 import BreakpointManager from '../../lib/debugger/breakpoint_manager'
+import DebuggerView from '../../lib/debugger/debugger_view'
 
 describe('Debugger', () => {
   describe('commands', () => {
     it('binds atom-agk:toggle-debugger to #toggleDebugger', () => {
       const disposables = new CompositeDisposable()
-      const breakpoints = new BreakpointManager(disposables)
-      const runner = new Runner(breakpoints)
-      const debugManager = new Debugger(disposables, runner)
+      const debugManager = new Debugger({ subscriptions: disposables })
       const workspaceElement = atom.views.getView(atom.workspace)
       spyOn(debugManager, 'toggleDebugger')
 
@@ -21,9 +20,7 @@ describe('Debugger', () => {
 
     it('binds "atom-agk:debug" to #debug', () => {
       const disposables = new CompositeDisposable()
-      const breakpoints = new BreakpointManager(disposables)
-      const runner = new Runner(breakpoints)
-      const debugManager = new Debugger(disposables, runner)
+      const debugManager = new Debugger({ subscriptions: disposables })
       const workspaceElement = atom.views.getView(atom.workspace)
       spyOn(debugManager, 'debug')
 
@@ -33,12 +30,22 @@ describe('Debugger', () => {
     })
   })
 
+  it('opens the view on atom://agk-debugger', () => {
+    const disposables = new CompositeDisposable()
+    const view = new DebuggerView()
+    new Debugger({ subscriptions: disposables, view: view })
+    spyOn(view, 'getDefaultLocation')
+
+    waitsForPromise(() => atom.workspace.open(view.getURI()))
+    runs(() => expect(view.getDefaultLocation).toHaveBeenCalled())
+  })
+
   describe('#debug', () => {
     it('starts the runner', () => {
       const disposables = new CompositeDisposable()
       const breakpoints = new BreakpointManager(disposables)
       const runner = new Runner(breakpoints)
-      const debugManager = new Debugger(disposables, runner)
+      const debugManager = new Debugger({ subscriptions: disposables, runner: runner })
       spyOn(runner, 'start')
 
       debugManager.debug()
