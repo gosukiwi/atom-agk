7cc6047f2aa6a3da80b9523c00ef11e752b0fa58
Federico Ramirez <fedra.arg@gmail.com>
2020-06-04 00:15:52 -0300

    Specs

---
+++ spec/debugger/debugger_view-spec.js
@@ -0,0 +1,33 @@
+'use babel'
+import DebuggerView from '../../lib/debugger/debugger_view'
+
+describe('DebuggerView', () => {
+  describe('.writeLine', () => {
+    it('adds a new line to the output', () => {
+      const view = new DebuggerView()
+
+      view.writeLine('Hello, World!')
+
+      expect(view.getElement().querySelector('.agk-debugger__output').innerHTML).toContain('Hello, World!')
+    })
+  })
+
+  describe('.clear', () => {
+    it('clear the output', () => {
+      const view = new DebuggerView()
+
+      view.writeLine('Hello, World!')
+      view.clear()
+
+      expect(view.getElement().querySelector('.agk-debugger__output').innerHTML).not.toContain('Hello, World!')
+    })
+
+    it('adds default text', () => {
+      const view = new DebuggerView()
+
+      view.clear()
+
+      expect(view.getElement().querySelector('.agk-debugger__output').innerHTML).toContain('AGK Debugger Console. Not connected.')
+    })
+  })
+})
