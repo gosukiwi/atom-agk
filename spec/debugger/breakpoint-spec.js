2f469c8c4ed0c9a8a9155c1df99619495c3b2b09
Federico Ramirez <fedra.arg@gmail.com>
2020-06-04 16:27:22 -0300

    Refactor

--- spec/debugger/breakpoint-spec.js
+++ spec/debugger/breakpoint-spec.js
@@ -6,7 +6,7 @@ describe('Breakpoint', () => {
   it('relativizes the file path', () => {
     const environment = new Environment()
     spyOn(environment, 'projectPath').andReturn('C:\\Foo')
-    const breakpoint = new Breakpoint('C:\\Foo\\Bar\\baz.agc', 1, null, environment)
+    const breakpoint = new Breakpoint({ file: 'C:\\Foo\\Bar\\baz.agc', line: 1, decoration: null, environment })
 
     expect(breakpoint.file).toBe('Bar\\baz.agc')
   })
