11dc33da9ab6e33dbe44f21beca562c693a792b9
Federico Ramirez <fedra.arg@gmail.com>
2020-06-04 17:39:44 -0300

    Refactor

--- spec/debugger/breakpoint_manager-spec.js
+++ spec/debugger/breakpoint_manager-spec.js
@@ -4,30 +4,33 @@ import BreakpointManager from '../../lib/debugger/breakpoint_manager'
 
 describe('BreakpointManager', () => {
   describe('commands', () => {
-    it('binds atom-agk:add-breakpoint to .add', () => {
+    it('binds atom-agk:toggle-breakpoint to .toggle', () => {
       const subscriptions = new CompositeDisposable()
       const manager = new BreakpointManager(subscriptions)
       const workspaceElement = atom.views.getView(atom.workspace)
-      spyOn(manager, 'add')
+      spyOn(manager, 'toggle')
 
-      atom.commands.dispatch(workspaceElement, 'atom-agk:add-breakpoint')
+      atom.commands.dispatch(workspaceElement, 'atom-agk:toggle-breakpoint')
 
-      expect(manager.add).toHaveBeenCalled()
+      expect(manager.toggle).toHaveBeenCalled()
     })
   })
 
-  describe('.add', () => {
-    it('adds a new breakpoint', () => {
+  describe('.toggle', () => {
+    it('toggles a breakpoint', () => {
       const subscriptions = new CompositeDisposable()
       const manager = new BreakpointManager(subscriptions)
 
       waitsForPromise(() => atom.workspace.open('foo.agc'))
       runs(() => {
-        manager.add()
+        manager.toggle()
         expect(manager.breakpoints.length).toBe(1)
         expect(manager.breakpoints[0].file).toBe('foo.agc')
         expect(manager.breakpoints[0].line).toBe(0)
         expect(manager.breakpoints[0].decoration).toBeDefined()
+
+        manager.toggle()
+        expect(manager.breakpoints.length).toBe(0)
       })
     })
   })
