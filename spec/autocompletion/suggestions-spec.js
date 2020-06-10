1a571a02c6bdd6a4cd37e95e2468f51beda6faf5
Federico Ramirez <fedra.arg@gmail.com>
2020-06-10 15:35:01 -0300

    Refactor

--- spec/autocompletion/suggestions-spec.js
+++ spec/autocompletion/suggestions-spec.js
@@ -1,13 +1,13 @@
 'use babel'
 import { CompositeDisposable } from 'atom'
 import Environment from '../../lib/environment'
-import Definitions from '../../lib/autocompletion/definitions'
+import Suggestions from '../../lib/autocompletion/suggestions'
 
-describe('Definitions', () => {
+describe('Suggestions', () => {
   it('loads built-in definitions', () => {
     const subscriptions = new CompositeDisposable()
     const environment = Environment.instance
-    const definitions = new Definitions({ subscriptions, environment })
+    const definitions = new Suggestions({ subscriptions, environment })
 
     expect(definitions.get('setspr').find((definition) => definition.name === 'SetSpriteAnimation')).not.toBeUndefined()
   })
--- spec/fixtures/demo.agc
+++ spec/fixtures/demo.agc
@@ -1,3 +1,3 @@
-Function Foo
+Function Foo()
   Print("Hi")
 EndFunction
