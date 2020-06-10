abc89cb1bb098c426af1905f3a9aebaae6dab19f
Federico Ramirez <fedra.arg@gmail.com>
2020-06-09 22:13:53 -0300

    Fix linter setup

---
+++ config.js
@@ -0,0 +1,8 @@
+export default {
+  agk_compiler_path: {
+    title: 'AGK Compiler Path',
+    description: 'The full path to the AGK compiler executable.',
+    type: 'string',
+    default: 'C:\\Program Files\\Steam\\steamapps\\common\\App Game Kit 2\\Tier 1\\Compiler\\AGKCompiler.exe'
+  }
+}
