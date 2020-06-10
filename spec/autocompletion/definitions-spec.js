503e58a490fe6da28902d8578130c75a4dfe9494
Federico Ramirez <fedra.arg@gmail.com>
2020-06-10 14:48:06 -0300

    Specs

--- spec/autocompletion/definitions-spec.js
+++ spec/autocompletion/definitions-spec.js
@@ -1,5 +1,14 @@
 'use babel'
+import { CompositeDisposable } from 'atom'
+import Environment from '../../lib/environment'
+import Definitions from '../../lib/autocompletion/definitions'
 
 describe('Definitions', () => {
-  it('works')
+  it('loads built-in definitions', () => {
+    const subscriptions = new CompositeDisposable()
+    const environment = Environment.instance
+    const definitions = new Definitions({ subscriptions, environment })
+
+    expect(definitions.get('setspr').find((definition) => definition.name === 'SetSpriteAnimation')).not.toBeUndefined()
+  })
 })
--- spec/spec-helper.js
+++ spec/spec-helper.js
@@ -2,5 +2,6 @@
 import path from 'path'
 
 export function fixture (name) {
+  if (name === undefined) return path.join(__dirname, 'fixtures')
   return path.join(__dirname, 'fixtures', name)
 }
